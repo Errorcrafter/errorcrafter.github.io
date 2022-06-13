8dd60c4444bbfa0f0153a91cd796a06186de4b78
Errorcrafter <xlmodder180@gmail.com>
2022-06-13 17:05:31 +0800

    a lot of things

--- assets/index.js
+++ assets/index.js
@@ -1,6 +1,6 @@
 function getMOTD() {
     const motds = ["motd moment","trolled!!!!!!!","the king of copy and paste","premium skidding services","allan please add details","minercaft maniac 😤😤😤",
-    "i have no idea what i'm doing like 99% of the time","DONT DO SE[double u]ER SIDE","#savetf2","python/js developer."];
+    "i have no idea what i'm doing like 99% of the time","DONT DO SE[double u]ER SIDE","#savetf2","python/js developer.","OOP has ruined me","datapacks >>> mods"];
     return motds[Math.floor(Math.random() * motds.length)];
 }
 
